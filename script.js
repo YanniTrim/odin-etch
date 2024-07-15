48f051a35c72a4d69dc665ac3887a413fdcab423
Yanni <yannitrim@gmail.con>
2024-07-15 16:45:02 -0400

    Added darken() support

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const containerDiv = document.querySelector("div");
+const containerDiv = document.querySelector("#main-contain");
 
 function hoverBlack(){
     this.style.backgroundColor = "black";
@@ -11,19 +11,33 @@ function hoverColor() {
     this.style.backgroundColor = `rgb(${redRand},${greenRand},${blueRand})`;
 }
 
+function darken() {
+    const redRand = Math.random()*255;
+    const greenRand = Math.random()*255;
+    const blueRand = Math.random()*255;
+    this.style.backgroundColor = `rgb(${redRand},${greenRand},${blueRand})`; //set random color
+    this.setAttribute("timesHovered",parseInt(this.getAttribute("timesHovered"))+1); // increase timesHovered by 1
+    console.log(this.getAttribute("timesHovered"));
+    this.style.opacity = 1 - (parseInt(this.getAttribute("timesHovered"))/10);
+
+}
+
 
 // Create 16x16 grid
 function createGrid(size) {
     for (let row = 0; row<size; row++) {
         const div = document.createElement("div");
+        document.createAttribute("timesHovered");
         div.classList.add("row");
         for (let col = 0; col < size; col++) {
             const subDiv = document.createElement("div");
             subDiv.classList.add("item");
-            subDiv.addEventListener("mouseover", hoverColor);
+            subDiv.setAttribute("timesHovered", 0);
+            subDiv.addEventListener("mouseover", darken);
             div.appendChild(subDiv);
         }
         containerDiv.appendChild(div);
+        containerDiv.style.backgroundColor = "black";
     }
 }
 
